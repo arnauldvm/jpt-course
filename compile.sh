5a3ac4c754f0ef26c4cabfd7eabbb6f929db0a0c
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-09-24 03:29:42 +0200

    add(build): add PDF generation

--- compile.sh
+++ compile.sh
@@ -4,12 +4,17 @@ srcpath="$1"
 
 srcbasedir=src/main/adoc
 tgtbasedir=build/html
+tgtpdfbasedir=build/pdf
 rootdir="$(pwd)"
 
 tgtpath="$(echo "$srcpath" |
   perl -pe 's#^\Q'"$rootdir"'\E\/##;
             s#^\Q'"$srcbasedir"'\E#'"$tgtbasedir"'#;
             s#\.adoc$#.html#')"
+
+tgtpdfpath="$(echo "$tgtpath" |
+  perl -pe 's#^\Q'"$tgtbasedir"'\E#'"$tgtpdfbasedir"'#;
+            s#\.html$#.pdf#')"
 
 if [ "$tgtpath" -nt "$srcpath" ]; then
   echo "$tgtpath already exists and is up to date, nothing to do."
@@ -19,9 +24,13 @@ fi
 tgtdir="$(dirname "$tgtpath")"
 mkdir -p "$tgtdir"
 
+tgtpdfdir="$(dirname "$tgtpdfpath")"
+mkdir -p "$tgtpdfdir"
+
 build_options="$(head -1 "$srcpath" |
   perl -pe 's#^//\s*build_options:\s*(.*)\s*$#$1# or $_=""')"
 
 set -x
 asciidoc $build_options -o "$tgtpath" "$srcpath"
+asciidoc $build_options -b html -o - "$srcpath" | wkhtmltopdf - "$tgtpdfpath"
 
