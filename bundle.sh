87f5efb3dc18114235db190b135531f9f435a65a
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-09-28 15:08:44 +0200

    add(build): script to build one big PDF bundle

---
+++ bundle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+find src/main/adoc -type f -name '*.adoc' | sort | perl -pe 's/^(.*)$/include::\1\[\]/' |\
+  asciidoc -a icons -d book -a toc2! -s -a numbered -o - - | \
+  wkhtmltopdf toc - build/pdf/jpt-bundle.pdf
+
